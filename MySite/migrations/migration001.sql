de939194ce72d62975246fb08dd60bcfde5000f6
OlgaStepanenko <42977722+OlgaStepanenko@users.noreply.github.com>
2018-12-08 01:57:04 +0300

    Add files via upload

---
+++ MySite/migrations/migration001.sql
@@ -0,0 +1,19 @@
+SELECT * FROM `duble_gallery` WHERE 1
+
+INSERT INTO duble_gallery (ID, name, path, gallery)
+VALUES 
+('1', 'foto_1', 'img/flex1.png', 'Первая галерея'),
+('2', 'foto_2', 'img/flex2.png', 'Первая галерея'),
+('3', 'foto_3', 'img/flex3.png', 'Первая галерея'),
+('4', 'foto_4', 'img/flex4.png', 'Первая галерея'),
+('5', 'foto_5', 'img/flex5.png', 'Первая галерея'),
+('6', 'foto_6', 'img/flex6.png', 'Первая галерея'),
+('7', 'foto_7', 'img/flex7.png', 'Первая галерея'),
+('8', 'foto_8', 'img/flex8.png', 'Первая галерея'),
+('9', 'foto_9', 'img/flex9.png', 'Первая галерея'),
+('1', 'foto_10', 'img/spb_gallery/spb1.jpg', 'Вторая галерея'),
+('2', 'foto_11', 'img/spb_gallery/spb2.jpg', 'Вторая галерея'),
+('3', 'foto_12', 'img/spb_gallery/spb3.jpg', 'Вторая галерея'),
+('4', 'foto_13', 'img/spb_gallery/spb4.jpg', 'Вторая галерея'),
+('5', 'foto_14', 'img/spb_gallery/spb5.jpg', 'Вторая галерея'),
+('6', 'foto_15', 'img/spb_gallery/spb6.jpg', 'Вторая галерея');
