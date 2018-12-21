b9b47a0493d4916eed09f0e2bacc0138aa4851b8
OlgaStepanenko <42977722+OlgaStepanenko@users.noreply.github.com>
2018-12-21 03:09:50 +0300

    Add files via upload

---
+++ SiteGallery/Test/public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<div class="container">
+		<header>
+		
+		</header>
+		<main>
+
+
+		
+		</main>
+		<footer>
+		
+		</footer>
+	</div>
+</body>
+</html>
